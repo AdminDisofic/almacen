68e15c6b57b803903d44f114d3d78c8f2f6aecbd
Miguel Angel Garcia <disoficmigue@gmail.com>
2023-06-15 13:32:59 +0200

    LMCN-8 Subir PictureBox con evento

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace HolaMundoGit.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Skylinne {
+            get {
+                object obj = ResourceManager.GetObject("Skylinne", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
