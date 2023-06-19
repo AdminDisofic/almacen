5f3361ba7c56568878f6f3af7c3a24b45d5935cb
Miguel Angel Garcia Fernandez <migue@DESKTOP-U24977S>
2023-06-19 16:41:35 +0200

    LMCN-9 Boton Migue creado con su evento

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -29,15 +29,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Migue = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
+            // btn_Migue
+            // 
+            this.btn_Migue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Migue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Migue.Location = new System.Drawing.Point(681, 391);
+            this.btn_Migue.Name = "btn_Migue";
+            this.btn_Migue.Size = new System.Drawing.Size(107, 47);
+            this.btn_Migue.TabIndex = 0;
+            this.btn_Migue.Text = "Migue";
+            this.btn_Migue.UseVisualStyleBackColor = false;
+            this.btn_Migue.Click += new System.EventHandler(this.btn_Migue_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-
+            this.Controls.Add(this.btn_Migue);
             this.Name = "Menu";
             this.Text = "Menu Hola Mundo GIT";
             this.ResumeLayout(false);
@@ -46,6 +58,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btn_Migue;
     }
 }
 
