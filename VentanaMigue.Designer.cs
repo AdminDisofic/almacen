f63ee7a1b151cbe378603010d4b16ce20e12ebdd
Miguel Angel Garcia Fernandez <migue@DESKTOP-U24977S>
2023-06-21 17:09:56 +0200

    LMCN-18 Cambios error

--- VentanaMigue.Designer.cs
+++ VentanaMigue.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(242, 202);
+            this.label1.Location = new System.Drawing.Point(253, 101);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(296, 46);
             this.label1.TabIndex = 0;
