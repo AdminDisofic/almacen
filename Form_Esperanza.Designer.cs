c1b79ba0d37f6217e669e1c62699c73a4b660631
Esperanza <Esperanza@DESKTOP-03A0N86>
2023-06-22 11:18:15 +0200

    Imagen introducida

--- Form_Esperanza.Designer.cs
+++ Form_Esperanza.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Esperanza));
             this.label1 = new System.Windows.Forms.Label();
+            this.Picture_Box_Esperanza = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Box_Esperanza)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -36,23 +39,34 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(410, 211);
+            this.label1.Location = new System.Drawing.Point(488, 80);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(298, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nuevo formulario Esperanza";
             // 
+            // Picture_Box_Esperanza
+            // 
+            this.Picture_Box_Esperanza.Image = ((System.Drawing.Image)(resources.GetObject("Picture_Box_Esperanza.Image")));
+            this.Picture_Box_Esperanza.Location = new System.Drawing.Point(493, 238);
+            this.Picture_Box_Esperanza.Name = "Picture_Box_Esperanza";
+            this.Picture_Box_Esperanza.Size = new System.Drawing.Size(285, 178);
+            this.Picture_Box_Esperanza.TabIndex = 1;
+            this.Picture_Box_Esperanza.TabStop = false;
+            // 
             // Form_Esperanza
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1733, 969);
+            this.Controls.Add(this.Picture_Box_Esperanza);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form_Esperanza";
             this.Text = "Form_Esperanza";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Box_Esperanza)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Picture_Box_Esperanza;
     }
 }
