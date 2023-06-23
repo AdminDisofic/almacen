085f92ed9d7724aba377e75ee9fbda45db8ab9e3
Miguel Angel Garcia <disoficmigue@gmail.com>
2023-06-23 09:39:01 +0200

    LMCN_24 DatagridVie agregado

--- VentanaMigue.Designer.cs
+++ VentanaMigue.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dgv_migue = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_migue)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(253, 33);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(303, 46);
             this.label1.TabIndex = 0;
@@ -46,23 +48,33 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::HolaMundoGit.Properties.Resources.Braco;
-            this.pictureBox1.Location = new System.Drawing.Point(214, 138);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 207);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(382, 231);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // dgv_migue
+            // 
+            this.dgv_migue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_migue.Location = new System.Drawing.Point(404, 207);
+            this.dgv_migue.Name = "dgv_migue";
+            this.dgv_migue.Size = new System.Drawing.Size(384, 231);
+            this.dgv_migue.TabIndex = 2;
+            // 
             // VentanaMigue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv_migue);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "VentanaMigue";
             this.Text = "VentanaMigue";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_migue)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +84,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dgv_migue;
     }
 }
